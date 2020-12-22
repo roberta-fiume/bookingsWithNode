2dcf1d9c327399e6d0172694e52cbee7e11655bd
theanimeoneesan.business <theanimeoneesan.business@gmail.com>
2020-12-22 18:20:28 +0000

    added post request and database dumps

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-
-
 // Listen to the App Engine-specified port, or 8080 otherwise
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
@@ -18,59 +16,53 @@ app.listen(PORT, () => {
 var mysql = require('mysql');
 
 var con = mysql.createConnection({
-  host: "35.242.165.143",
+    //   host: "35.242.165.143",
+  host: "35.235.54.201",
   port: 3306,
-  user: "test",
+    //   user: "test",
+  user: "roberta",
   password: "1234",
   database: "projects_database"
 });
 
-con.connect(function(err) {
-    try {
-        if (err) throw err;
-        console.log("Connected!");
-        var sql = "INSERT INTO bookings (firstname, lastname, email) VALUES ('Giovanni', 'Battista', 'g.b.j@outlook.com')";
-        con.query(sql, function (err, result) {
-        if (err) throw err;
-            console.log("1 record inserted");
-        });  
-    } catch (error) {
-        console.log("ERROR CONNECTING TO THE DATABASE", error);
+con.connect();
+
+const users = app.get('/', (req, res) => {
+    con.query('SELECT * FROM bookings', (err, rows, fields) => {
+        if (!err)
+        res.send(rows);
+        else 
+        console.log(err);
+    })
+});
+
+  
+app.post('/user', function (req, res) {
+
+    let user = {
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email
     }
+    con.query("INSERT INTO bookings SET ?", user, function (error, results) {
+        if (error) throw error;
+        console.log("this is the error", error);
+        return res.send(user);
+    });
 });
 
+app.put('/user/:id', (req, res) => {
+    let id = req.params.id;
 
-app.get('/', (req, res) => {
- con.query('SELECT * FROM bookings', (err, rows, fields) => {
-     if (!err)
-     res.send(rows);
-     else 
-     console.log(err);
- })
-});
+ res.send("user modified!");
+})
 
-
-
-
-
-
-// app.post('/user', urlencodedParser, function (req, res) {
-//     console.log(req.body);
-//     var firstname = req.body.firstname;
-//     var lastname = req.body.lastname;
-//     var email = req.body.email;
-
-//     con.connect(function (err) {
-//         if (err) throw err;
-//         console.log("connected");
-
-//         var sql = "INSERT INTO bookings (firstname, lastname, email) VALUES ('"+Giovanni+",  "+Battista+", "+giovanni.battistaoutlook.com+")"; 
-//         con.query(sql, function (err) {
-//             if (err) throw err;
-//             console.log("Other record inserted");
+// app.put('/user', function (req, res) {
+//     con.query('UPDATE `employee` SET `employee_name`=?,`employee_salary`=?,`employee_age`=? where `id`=?', [req.body.employee_name,req.body.employee_salary, req.body.employee_age, req.body.id], function (error, results, fields) {
+//         if (error) throw error;
+//         res.end(JSON.stringify(results));
 //         });
-//     });
-//     res.render('user added', { data: req.body });
 // });
+
 
 

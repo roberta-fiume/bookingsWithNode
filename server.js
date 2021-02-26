23d916fab6a072b4455b0516190c394135d8745e
roberta-fiume <robertaf898@gmail.com>
2021-02-26 16:37:44 +0000

    added cors and delete request

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
+const cors = require('cors');
 var bodyParser = require('body-parser');
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.options('*', cors());
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
 
 
 // Listen to the App Engine-specified port, or 8080 otherwise
@@ -33,6 +41,17 @@ app.put('/user/:id', (req, res) => {
         where: { id: id }
     }).then(user => res.send(user)
     );
+});
+
+app.delete('/user/:id', (req, res) => {
+    const id = req.params.id;
+    Bookings.destroy({
+        where: { id: id }
+    }).then(user => res.send("Your booking has been canceled successfully!")
+    )
+    .catch( err => {
+        res.status(500).send("Oops...Something went wrong!");
+    });
 })
 
 
